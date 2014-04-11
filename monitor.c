369e3a265ce5bb8578bafb7af0b440f4b785d04e
Diogo Barroso <ei11105@fe.up.pt>
2014-04-11 18:32:11 +0100

    file open

--- monitor.c
+++ monitor.c
@@ -2,19 +2,31 @@
 
 int main(int argc, char* argv[])
 {
-	
+	int nFiles = argc - 3;
+	pid_t pid[nFiles];
+	int index = 0;
+
 	int pf[2]; //pipe pai-filho
 	int i = 0;
 
 	pipe(fd);
 
-	int mkfifo( 'FIFO' , 
-	for(i; i < argc; i++){
-		pid = fork();
+	int mkfifo( 'FIFO' , 'chmod');
+	for(i; i < nFiles; i++){
+		pid[i] = fork();
 
-		if(pid > 0){
+		if(pid[i] > 0){
 			close(pf[0]); //fecha leitura pipe pai-filho
 		} else {
+			index = i + 3;
 			close(pf[1]); //fecha escrita pipe pai-filho
 		}
 	}
+
+	i = 0;
+
+	if( index > 0) {
+		FILE *file = *fopen("%s" argv[index], "r");
+	}
+
+	
