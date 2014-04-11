b8e35be7eccc8cdeee229a16201533f9999ba4fe
Diogo Barroso <ei11105@fe.up.pt>
2014-04-11 19:26:44 +0100

    It compiles and shows the children PID, there are created as much as needed. Debug lines left, to confirm that

--- monitor.c
+++ monitor.c
@@ -1,17 +1,21 @@
-
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
 	int nFiles = argc - 3;
 	pid_t pid[nFiles];
 	int index = 0;
+	int myPid;
 
 	int pf[2]; //pipe pai-filho
 	int i = 0;
 
-	pipe(fd);
+	pipe(pf);
 
-	int mkfifo( 'FIFO' , 'chmod');
+	//int mkfifo( 'FIFO' , 'chmod'); //VERIFICAR INSTRUCAO
 	for(i; i < nFiles; i++){
 		pid[i] = fork();
 
@@ -19,6 +23,7 @@ int main(int argc, char* argv[])
 			close(pf[0]); //fecha leitura pipe pai-filho
 		} else {
 			index = i + 3;
+			myPid = getpid();
 			close(pf[1]); //fecha escrita pipe pai-filho
 		}
 	}
@@ -26,7 +31,11 @@ int main(int argc, char* argv[])
 	i = 0;
 
 	if( index > 0) {
-		FILE *file = *fopen("%s" argv[index], "r");
+		FILE *file = fopen( argv[index], "r");
+		printf("I'm process number %d", myPid);
 	}
+	
 
+	return 0;
+}
 	
